e7df8656d0cd67788e2a39cd413b6927721d393f
puspitasahu <puspita5sahu@gmail.com>
2026-03-09 09:52:57 +0530

    Jdbc Based Chat Memory

--- src/main/java/com/openai/config/ChatConfiguration.java
+++ src/main/java/com/openai/config/ChatConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +15,15 @@ import java.util.List;
 
 @Configuration
 public class ChatConfiguration {
+
+   @Bean
+   public ChatMemory chatMemory(JdbcChatMemoryRepository jdbcChatMemoryRepository){
+        return MessageWindowChatMemory.builder()
+                .maxMessages(5)
+                .chatMemoryRepository(jdbcChatMemoryRepository)
+                .build();
+
+    }
 
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatmemory){
